e4a2cb656d90dd43f51fff690005e6ec81448ac9
Luscarvalho <39837517+Luscarvalho@users.noreply.github.com>
2019-10-29 11:38:38 -0300

    Card no Feed

--- UflaNewsKND/src/app/model/noticia.model.ts
+++ UflaNewsKND/src/app/model/noticia.model.ts
@@ -3,14 +3,14 @@ import { ModeloTopico } from './topico.model';
 import { ModeloComentario } from './comentario.model';
 
 export class ModeloNoticia {
-    constructor(
-        public capa: string,
-        public publicador: ModeloPublicador,
-        public titulo: string,
-        public likes: number,
-        public artigo: string,
-        public topicos: ModeloTopico[],
-        public data: Date,
-        public comentarios: ModeloComentario[],
-    ) {}
+  constructor(
+    public capa: string,
+    public publicador: ModeloPublicador,
+    public titulo: string,
+    public likes: number,
+    public artigo: string,
+    public topicos: ModeloTopico[],
+    public data: Date,
+    public comentarios: ModeloComentario[],
+  ) {}
 }
